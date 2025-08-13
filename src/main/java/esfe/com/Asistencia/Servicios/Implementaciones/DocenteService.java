85432923cdaaaa6ce5d687233125301b7b32de20
cindy hernandez <hr25001@esfe.agape.edu.sv>
2025-08-13 14:16:24 -0600

    Se realizo Docentes

---
+++ src/main/java/esfe/com/Asistencia/Servicios/Implementaciones/DocenteService.java
@@ -0,0 +1,43 @@
+package esfe.com.Asistencia.Servicios.Implementaciones;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import esfe.com.Asistencia.Modelos.Docente;
+import esfe.com.Asistencia.Repositorios.IDocenteRepository;
+import esfe.com.Asistencia.Servicios.Interfaces.IDocenteService;
+
+@Service
+public class DocenteService implements IDocenteService{
+ @Autowired
+    private IDocenteRepository docenteRepository;
+
+    @Override
+    public Page<Docente> buscarTodosPaginados(Pageable pageable) {
+        return docenteRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<Docente> obtenerTodos() {
+        return docenteRepository.findAll();
+    }
+
+    @Override
+    public Optional<Docente> buscarPorId(Integer id) {
+        return docenteRepository.findById(id);
+    }
+
+    @Override
+    public Docente crearOEditar(Docente docente) {
+        return docenteRepository.save(docente);
+    }
+
+    @Override
+    public void eliminarPorId(Integer id) {
+        docenteRepository.deleteById(id);
+    }
+}
